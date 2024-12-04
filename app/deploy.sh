aadb5e0d80af14f6d16b6c7febac11f1c77d79fb
ddezano <david.dezano@harmonicinc.com>
2024-12-04 18:00:47 +0200

    made function for rc check

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,55 +1,45 @@
 #!/bin/bash
+#
 
+# Exit on rc != 0
+check_rc () {
+  if [ $? -ne 0 ]; then
+    echo "Error: failed with return code $?"
+    exit $?
+  fi
+}
 
 # Install Essentials
 echo "Installing Python3, Git, and PIP..."
 sudo apt update && sudo apt install -y python3 && sudo apt install -y pip && sudo apt install -y git
-if [ $? -ne 0 ]; then
-  echo "Error: Installing Essentials failed with return code $?"
-  exit $?
-fi
+check_rc
 
 # Clone Repository
 echo "Cloning repository..."
 chdir /home/ubuntu/
 git clone https://github.com/odedra35/dms
-if [ $? -ne 0 ]; then
-  echo "Error: Cloning Repository failed with return code $?"
-  exit $?
-fi
+check_rc
 
 # Check Repository dir
 echo "Checking dms/app dir is present..."
 test -f dms/app
-if [ $? -ne 0 ]; then
-  echo "Error: Could not find folder dms/app..."
-  exit $?
-fi
+check_rc
 
 # Switch to dev branch
 echo "Switch to dev branch..."
 chdir /home/ubuntu/dms/app
 git switch dev
-if [ $? -ne 0 ]; then
-  echo "Error: Could not git switch to dev branch..."
-  exit $?
-fi
+check_rc
 
 # Install python packages
 echo "Installing requirements.txt (breaking)..."
 sudo pip install -r requiremtns.txt --break-system-packages
-if [ $? -ne 0 ]; then
-  echo "Error: Installing python packages failed with return code $?"
-  exit $?
-fi
+check_rc
 
 # Unblock 8080/tcp port using ufw
 echo "Unblock ufw port 8080..."
 sudo ufw allow 8080/tcp
-if [ $? -ne 0 ]; then
-  echo "Error: Unblocking ufw port 8080 failed with return code $?"
-  exit $?
-fi
+check_rc
 
 # Run APP in bg
 echo "Starting routes.py..."
